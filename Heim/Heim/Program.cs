6cd38055203b6a4fece5e55279b2ae6e1cb91e43
Manai <abdoumanai1987@hotmail.com>
2022-11-05 10:26:26 +0100

    Update Program.cs

--- Heim/Heim/Program.cs
+++ Heim/Heim/Program.cs
@@ -1,40 +1,46 @@
-﻿namespace Heim;
+namespace Heim;
 
 internal class Program
 {
-    public static MyList<int> My_list { get; set; } = new();
+    public static MyList<int> My_list { get; set; }
 
     private static void Main(string[] args)
     {
-        //Adding value Test
-        InitializeTest("Adding value");
-        Console.WriteLine($"Value to Add: 19 ");
-        My_list.Add(19);
-        DisplayListAfterAction("List data after adding 19: ");
+        //Adding values Test
+        InitializeTest("Adding value", 5);
+        My_list.Add(15);
+        Console.WriteLine($"Value to Add: 15 ");
+        DisplayListAfterAction("List data after adding:  ");
 
         //Clearing value Test
-        InitializeTest("Clearing list");
-        Console.WriteLine($"Actual list length: {My_list.Count()}");
+        InitializeTest("Clearing list", 6);
         My_list.Clear();
-        Console.WriteLine($"Clearing list..., new list length: {My_list.Count()}");
+        DisplayListAfterAction("Clearing list... List data after clearing:  ");
 
         //Inserting value Test
-        InitializeTest("Inserting list");
-        Console.WriteLine($"Want to insert 99 between 3 and 4");
+        InitializeTest("Inserting list", 5);
+        Console.WriteLine($"Want to insert 99 at index 2");
         My_list.Insert(2, 99);
         DisplayListAfterAction("list datas after inserting : ");
 
         //Removing value Test
-        InitializeTest("Removeing from list");
-        Console.WriteLine("Want to Remove '5' value ");
-        My_list.Remove(5);
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine($"********* Removeing from list Test *********");
+
+        My_list = new MyList<int>(5);
+        FillArray();
+        My_list[0] = 4;
+        DisplayInitialList();
+        Console.WriteLine("Want to Remove '4' value ");
+        My_list.Remove(4);
         DisplayListAfterAction($"list datas after removing '4' : ");
         Console.WriteLine("Want to Remove an non existing value '365'");
         My_list.Remove(365);
         DisplayListAfterAction($"list datas after removing non existing'365' : ");
 
         //RemoveingAt value Test
-        InitializeTest("RemoveingAt from list");
+        InitializeTest("RemoveingAt from list", 5);
         Console.WriteLine("Want to Remove at index '1'");
         My_list.RemoveAt(1);
         DisplayListAfterAction($"list datas after removingAt index '1' : ");
@@ -42,35 +48,35 @@ internal class Program
         My_list.RemoveAt(111);
 
         //IndexOf Test
-        InitializeTest("IndexOf");
+        InitializeTest("IndexOf", 5);
         Console.WriteLine("Want to get indexOf '5' and non existing '8' ");
         Console.WriteLine($"Index of value '5': {My_list.IndexOf(5)}");
         Console.WriteLine($"Index of value '8': {My_list.IndexOf(8)}");
 
         //Contains Test
-        InitializeTest("Contains");
+        InitializeTest("Contains", 5);
         Console.WriteLine("Want to check if the list contains '3' value and non existing'10' value ");
         Console.WriteLine($"Contains value '3': {My_list.Contains(3)}");
         Console.WriteLine($"Contains value '10': {My_list.Contains(13)}");
 
         //GetEnumerator Test
-        InitializeTest("GetEnumerator");
-        Console.WriteLine($"In my code i added four values. The 'GetEnumerator' is called for example from this method Console.WriteLine(string.Join(\"\\t\", Mylist));");
+        InitializeTest("GetEnumerator", 5);
+        Console.WriteLine($"In my code i added 5 values. The 'GetEnumerator' is called for example from this method Console.WriteLine(string.Join(\"\\t\", Mylist));");
         DisplayListAfterAction($"List datas after calling GetEnumerator : ");
 
         //Count Test
-        InitializeTest("Count");
+        InitializeTest("Count", 5);
         Console.WriteLine($"Calling Count : {My_list.Count} ");
 
         // this[int index] Get Test
-        InitializeTest("this[int index] Get ");
-        Console.WriteLine($"Want to get the value of index 2 : Mylist[2] : {My_list[2]} ");
-        Console.WriteLine($"Want to get the value of non existing index 1000 : Mylist[1000]");
+        InitializeTest("this[int index] Get ", 5);
+        Console.WriteLine($"Want to get the value of index 2, Mylist[2] : {My_list[2]} ");
+        Console.WriteLine($"Want to get the value of non existing index 1000, Mylist[1000]");
         int value = My_list[1000];
 
         // this[int index] Set Test
-        InitializeTest("this[int index] Set");
-        Console.WriteLine($"Want to Set the value of index 2 to 555: Mylist[2]");
+        InitializeTest("this[int index] Set", 5);
+        Console.WriteLine($"Want to Set the value of index 2 to 555: Mylist[2]=555");
         My_list[2] = 555;
         DisplayListAfterAction($"New list datas : ");
         Console.WriteLine($"Want to set the value of non existing index 1000 : Mylist[1000]=3");
@@ -78,16 +84,19 @@ internal class Program
         Console.Read();
     }
 
-    private static void InitializeTest(string testname)
+    private static void InitializeTest(string testname, int length)
     {
-        My_list.Clear();
-        My_list.Add(3);
-        My_list.Add(4);
-        My_list.Add(5);
-        My_list.Add(6);
         Console.WriteLine();
         Console.WriteLine();
         Console.WriteLine($"********* {testname} Test *********");
+
+        My_list = new MyList<int>(length);
+        FillArray();
+        DisplayInitialList();
+    }
+
+    private static void DisplayInitialList()
+    {
         Console.Write($"Actual list datas : ");
         Console.WriteLine(string.Join("\t", My_list));
     }
@@ -96,5 +105,13 @@ internal class Program
     {
         Console.Write(message);
         Console.WriteLine(string.Join("\t", My_list));
+    }
+
+    private static void FillArray()
+    {
+        for (int i = 0; i < My_list.Count; i++)
+        {
+            My_list[i] = i + 1;
+        }
     }
 }
